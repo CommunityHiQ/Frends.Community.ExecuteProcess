bcbb35f58e6065b0896a755c1553697ca6020ffc
narnil <narnil7@yahoo.com>
2017-11-10 14:19:39 +0200

    FCOM-23 Initial commit of migrated code

---
+++ Frends.Community.ExecuteProcess.Tests/ExecuteProcessTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Threading;
+
+namespace Frends.Community.ExecuteProcess.Tests
+{
+    [TestFixture]
+    public class ExecuteTests
+    {
+        private readonly string _testDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"ExecTests\");
+        private readonly string _process = Environment.ExpandEnvironmentVariables(@"%windir%\system32\cmd.exe");
+
+        [SetUp]
+        public void Setup()
+        {
+            if (!Directory.Exists(_testDir))
+                Directory.CreateDirectory(_testDir);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (Directory.Exists(_testDir))
+            {
+                Directory.Delete(_testDir, true);
+            }
+        }
+
+        [Test]
+        public void TestExecuteScript()
+        {
+            var testFileWithPath = Path.Combine(_testDir, @"test1.txt");
+
+            var args = new[]
+            {
+                new ExecuteProcessCommand.Argument { Name = "/C", Value = "echo testi >> " + testFileWithPath }
+            };
+            var input = new ExecuteProcessCommand.Input {ScriptPath = _process,  Arguments = args, WaitForResponse =  true, TimeoutMS = 6000 };
+
+            ExecuteProcessCommand.ExecuteProcess(input);
+
+            Assert.IsTrue(File.Exists(testFileWithPath)); // Tests if the file is created
+
+            Assert.AreEqual(File.ReadAllText(testFileWithPath), "testi " + Environment.NewLine); // Tests that the cmd.exe is only executed once
+        }
+
+        [Test]
+        public void TestExecuteMultipleArgs()
+        {
+            var testFileWithPath = Path.Combine(_testDir, @"test2.txt");
+
+            var args = new []
+            {
+                new ExecuteProcessCommand.Argument { Name = "/C", Value = "set" },
+                new ExecuteProcessCommand.Argument { Name = "/A", Value = "(1+10)" },
+                new ExecuteProcessCommand.Argument { Name = ">>", Value = testFileWithPath }
+            };
+
+            var input = new ExecuteProcessCommand.Input { ScriptPath = _process, Arguments = args, WaitForResponse = true, TimeoutMS = 6000 };
+
+            ExecuteProcessCommand.ExecuteProcess(input);
+
+            Assert.AreEqual(File.ReadAllText(testFileWithPath), "11");
+        }
+
+        [Test]
+        public void TestExecuteResult()
+        {
+            var args = new []
+            {
+                new ExecuteProcessCommand.Argument { Name = "/C", Value = "set" },
+                new ExecuteProcessCommand.Argument { Name = "/A", Value = "(1111+2222)" }
+            };
+
+            var input = new ExecuteProcessCommand.Input { ScriptPath = _process, Arguments = args, WaitForResponse = true, TimeoutMS = 6000 };
+
+            var result = ExecuteProcessCommand.ExecuteProcess(input);
+
+            Assert.IsTrue(result.Result.Contains("3333"));
+        }
+
+        [Test]
+        public void TestExecuteScriptsAsync()
+        {
+            var testFileWithPath = Path.Combine(_testDir, @"test3.txt");
+         
+            var args = new[]
+            {
+                new ExecuteProcessCommand.Argument { Name = "/C", Value = "set" },
+                new ExecuteProcessCommand.Argument { Name = "/A (1+10) >>", Value = testFileWithPath}
+            };
+            var input = new ExecuteProcessCommand.Input { ScriptPath = _process, Arguments = args, WaitForResponse = false };
+            var result = ExecuteProcessCommand.ExecuteProcess(input);
+
+            Assert.IsTrue(result.Status);
+            Thread.Sleep(100);
+            Assert.AreEqual(File.ReadAllText(testFileWithPath), "11");
+        }
+    }
+}
---
+++ Frends.Community.ExecuteProcess/ExecuteProcess.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Frends.Community.ExecuteProcess
+{
+    /// <summary>
+    /// Execute process
+    /// </summary>
+    public class ExecuteProcessCommand
+    {
+        /// <summary>
+        /// Argument
+        /// </summary>
+        public class Argument
+        {
+            /// <summary>
+            /// Argument name
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// Argument value
+            /// </summary>
+            public string Value { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Input
+        {
+            /// <summary>
+            /// Script path
+            /// </summary>
+            public string ScriptPath { get; set; }
+            /// <summary>
+            /// Arguments used
+            /// </summary>
+            public Argument[] Arguments { get; set; }
+            /// <summary>
+            /// Wait for response
+            /// </summary>
+            public bool WaitForResponse { get; set; }
+            /// <summary>
+            /// Timeout in milliseconds
+            /// </summary>
+            public int TimeoutMS { get; set; }
+        }
+
+        /// <summary>
+        /// Return object
+        /// </summary>
+        public class Output
+        {
+            /// <summary>
+            /// Request result
+            /// </summary>
+            public string Result { get; set; }
+            /// <summary>
+            /// Execute status
+            /// </summary>
+            public bool Status { get; set; }
+        }
+        
+        /// <summary>
+        /// Execute process
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static Output ExecuteProcess(Input input)
+        {
+            return input.WaitForResponse ? ExecuteProcessWithResult(input) : ExecuteProcessWithoutResult(input);
+        }
+
+        /// <summary>
+        /// Execute process and wait for result
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="ApplicationException"></exception>
+        /// <exception cref="TimeoutException"></exception>
+        private static Output ExecuteProcessWithResult(Input input)
+        {
+             var processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                Arguments = string.Join(" ", input.Arguments.Select(x => x.Name + " " + x.Value).ToArray()),
+                FileName = input.ScriptPath,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true
+            };
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                var output = new StringBuilder();
+                var error = new StringBuilder();
+
+                using (var outputWaitHandle = new AutoResetEvent(false))
+                using (var errorWaitHandle = new AutoResetEvent(false))
+                {
+                    void ProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
+                    {
+                        try
+                        {
+                            if (e.Data == null)
+                            {
+                                outputWaitHandle.Set();
+                            }
+                            else
+                            {
+                                output.AppendLine(e.Data);
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Trace.TraceError($"Error while executing process {input.ScriptPath} and handling output: {exception}");
+                        }
+                    }
+
+                    void ProcessOnErrorDataReceived(object sender, DataReceivedEventArgs e)
+                    {
+                        try
+                        {
+                            if (e.Data == null)
+                            {
+                                errorWaitHandle.Set();
+                            }
+                            else
+                            {
+                                error.AppendLine(e.Data);
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Trace.TraceError($"Error while executing process {input.ScriptPath} and handling error output: {exception}");
+                        }
+                    }
+
+                    try
+                    {
+                        process.OutputDataReceived += ProcessOnOutputDataReceived;
+                        process.ErrorDataReceived += ProcessOnErrorDataReceived;
+
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+
+                        if (process.WaitForExit(input.TimeoutMS) && outputWaitHandle.WaitOne(input.TimeoutMS) && errorWaitHandle.WaitOne(input.TimeoutMS))
+                        {
+                            if (process.ExitCode > 0)
+                            {
+                                throw new ApplicationException("External process execution failed with returncode: " + process.ExitCode + " and output: " + Environment.NewLine + error);
+                            }
+                            else
+                            {
+                                return new Output { Result = "External process execution was successful with output: " + Environment.NewLine + output, Status = true };
+                            }
+                        }
+                        else
+                        {
+                            throw new TimeoutException("External process execution timed out after " + input.TimeoutMS + " milliseconds.");
+                        }
+                    }
+                    finally
+                    {
+                        process.OutputDataReceived -= ProcessOnOutputDataReceived;
+                        process.ErrorDataReceived -= ProcessOnErrorDataReceived;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute process asynchronously
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static Output ExecuteProcessWithoutResult(Input input)
+        {
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                Arguments = string.Join(" ", input.Arguments.Select(x => x.Name + " " + x.Value).ToArray()),
+                FileName = input.ScriptPath,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true
+            };
+
+            using (var process = new Process())
+            {
+                process.StartInfo = processStartInfo;
+                return new Output { Status = process.Start() };
+            }
+        }
+    }
+}
